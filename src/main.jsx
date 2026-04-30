58e64af7ead35017d8954383973ed40f6da8e61e
Poseidros <sekosamuel@gmail.com>
2026-04-30 13:44:21 +0100

    Wrap application in WalletProvider

--- src/main.jsx
+++ src/main.jsx
@@ -45,6 +45,7 @@ function App() {
   }, [])
 
   return (
+    <WalletProvider>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
@@ -67,6 +68,7 @@ function App() {
         }}
       />
     </QueryClientProvider>
+    </WalletProvider>
   )
 }
 
